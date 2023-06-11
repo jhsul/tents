6e950c318b503e08e16aed8446beb54b1d49cff7
Jack Sullivan <jhaldisul@gmail.com>
2023-06-11 18:33:46 -0400

    ran some benchmarks, made some charts

--- py/benchmarks/addition.py
+++ py/benchmarks/addition.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch as th
 import time
 
-device = th.device("mps")
+device = th.device("cuda:0")
 
 
 def vecadd_forloop_cpu(n: int) -> float:
@@ -11,34 +11,34 @@ def vecadd_forloop_cpu(n: int) -> float:
 
     c = np.zeros(n)
 
-    start = time.time()
+    start = time.time_ns()
 
     for i in range(n):
         c[i] = a[i] + b[i]
 
-    return time.time() - start
+    return (time.time_ns() - start) / 1e6
 
 
 def vecadd_numpy_cpu(n: int) -> float:
     a = np.random.rand(n)
     b = np.random.rand(n)
 
-    start = time.time()
+    start = time.time_ns()
 
     c = a + b
 
-    return time.time() - start
+    return (time.time_ns() - start) / 1e6
 
 
 def vecadd_torch_cpu(n: int) -> float:
     a = th.rand(n)
     b = th.rand(n)
 
-    start = time.time()
+    start = time.time_ns()
 
     c = a + b
 
-    return time.time() - start
+    return (time.time_ns() - start) / 1e6
 
 
 def vecadd_torch_gpu(n: int) -> float:
@@ -46,8 +46,8 @@ def vecadd_torch_gpu(n: int) -> float:
     a = th.rand(n)
     b = th.rand(n)
 
-    start = time.time()
+    start = time.time_ns()
 
     c = a.to(device) + b.to(device)
 
-    return time.time() - start
+    return (time.time_ns() - start) / 1e6
--- py/benchmarks/matmul.py
+++ py/benchmarks/matmul.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch as th
 import time
 
-device = th.device("mps")
+device = th.device("cuda:0")
 
 
 def matmul_forloop_cpu(n: int) -> float:
@@ -11,7 +11,7 @@ def matmul_forloop_cpu(n: int) -> float:
 
     out = np.zeros((n, n))
 
-    start = time.time()
+    start = time.time_ns()
 
     for r in range(n):
         for c in range(n):
@@ -24,41 +24,88 @@ def matmul_forloop_cpu(n: int) -> float:
     # act = np.matmul(a, b)
     # print(act)
 
-    return time.time() - start
+    return (time.time_ns() - start) / 1e6
 
 
 def matmul_numpy_cpu(n: int) -> float:
     a = np.random.rand(n, n)
     b = np.random.rand(n, n)
 
-    start = time.time()
+    start = time.time_ns()
 
     c = np.matmul(a, b)
 
-    return time.time() - start
+    return (time.time_ns() - start) / 1e6
 
 
 def matmul_torch_cpu(n: int) -> float:
-    a = th.rand(n)
-    b = th.rand(n)
+    a = th.rand((n, n))
+    b = th.rand((n, n))
 
-    start = time.time()
+    start = time.time_ns()
 
     c = th.matmul(a, b)
 
-    return time.time() - start
+    return (time.time_ns() - start) / 1e6
 
 
 def matmul_torch_gpu(n: int) -> float:
-    a = th.rand(n)
-    b = th.rand(n)
+    a = th.rand((n, n))
+    b = th.rand((n, n))
 
-    start = time.time()
+    start = time.time_ns()
 
     c = th.matmul(a.to(device), b.to(device))
 
-    return time.time() - start
+    return (time.time_ns() - start) / 1e6
+
+
+def _batch_matmul_torch_cpu(n: int, size: int) -> float:
+    a = th.rand((n, size, size))
+    b = th.rand((n, size, size))
+
+    start = time.time_ns()
+
+    c = th.matmul(a, b)
+
+    return (time.time_ns() - start) / 1e6
+
+
+def _batch_matmul_torch_gpu(n: int, size: int) -> float:
+    a = th.rand((n, size, size))
+    b = th.rand((n, size, size))
+
+    start = time.time_ns()
+
+    c = th.matmul(a.to(device), b.to(device))
+
+    return (time.time_ns() - start) / 1e6
+
+
+def batch_matmul_4_torch_gpu(n: int) -> float:
+    return _batch_matmul_torch_gpu(n, 4)
+
+
+def batch_matmul_64_torch_gpu(n: int) -> float:
+    return _batch_matmul_torch_gpu(n, 64)
+
+
+def batch_matmul_256_torch_gpu(n: int) -> float:
+    return _batch_matmul_torch_gpu(n, 256)
+
+
+def batch_matmul_4_torch_cpu(n: int) -> float:
+    return _batch_matmul_torch_cpu(n, 4)
+
+
+def batch_matmul_64_torch_cpu(n: int) -> float:
+    return _batch_matmul_torch_cpu(n, 64)
+
+
+def batch_matmul_256_torch_cpu(n: int) -> float:
+    return _batch_matmul_torch_cpu(n, 256)
 
 
 if __name__ == "__main__":
-    matmul_forloop_colwise_cpu(3)
+    pass
+    # matmul_for_cpu(3)
