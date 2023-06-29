f160291780a6653f601c791d05dd680fee5fedc5
Jack Sullivan <me@jackhsullivan.com>
2023-06-29 11:39:10 -0400

    brought back basd toplevel await

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,7 @@ declare global {
   type VideoFrame = any;
 }
 
-// This should be in here
-// But it breaks and I can't use webgpu anyway
-// Oh well :(
-// await Tensor.setupDevice();
+await Tensor.setupDevice();
 
 export { Tensor } from "./tensor";
 export { Module, Linear } from "./nn";
